3ac3d61690c7ce991fae2523c210e448efd149a4
showa_yojyo <yojyo@hotmail.com>
2014-09-09 22:14:01 +0900

    (mkbashwrapper.py): run pylint.

--- mkbashwrapper.py
+++ mkbashwrapper.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Copyright (c) 2012 プレハブ小屋 <yojyo@hotmail.com>
-All Rights Reserved.  NO WARRANTY.
+"""Generate shell scripts that run Python scripts.
+
+Example:
+  You run this with
 """
 
 import sys
@@ -9,9 +12,10 @@ import glob
 from argparse import ArgumentParser
 from jinja2 import Environment
 
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 
-NEWLINE_SEQUENCE='\n'
+# Constant values.
+NEWLINE_SEQUENCE = '\n'
 PATTERN = '*.py'
 BASH_TEMPLATE = '''\
 #!/bin/bash
@@ -19,39 +23,66 @@ BASH_TEMPLATE = '''\
 /usr/bin/env python '{{ PY_SCRIPT_NAME }}' $@
 '''
 
+def configure():
+    """Parse the command line parameters.
+
+    Returns:
+        An instance of argparse.ArgumentParser that stores the command line
+        parameters.
+    """
+
+    parser = ArgumentParser(
+        description="Generate shell scripts for Python scripts.")
+    parser.add_argument('--version', action='version', version=__version__)
+    parser.add_argument(
+        '-d', '--directory',
+        default='.',
+        help='target directory (default to current directory)')
+
+    return parser
+
 def main(args):
-    # get target directory
+    """The main function.
+
+    Args:
+        args: An instance of argparse.ArgumentParser parsed in the configure
+            function.
+
+    Returns:
+        None.
+    """
+
     directory = args.directory
     os.chdir(directory)
 
-    dir = os.path.realpath(directory)
+    workdir = os.path.realpath(directory)
 
     env = Environment(newline_sequence=NEWLINE_SEQUENCE)
     template = env.from_string(BASH_TEMPLATE)
 
     for name in glob.iglob(PATTERN):
-        process(template, dir, name)
+        process(template, workdir, name)
 
-def process(template, dir, pyname):
-    try:
-        name, ext = os.path.splitext(pyname)
-        if ext == '.py':
-            shname = os.path.join(dir, name + '.sh')
-            kwargs = dict(PY_SCRIPT_NAME=os.path.realpath(pyname))
-            with open(shname, 'wb') as fout:
-                fout.write(
-                    template.render(**kwargs))
-                fout.write(NEWLINE_SEQUENCE)
-                sys.stderr.write('Processed {0}\n'.format(shname))
+def process(template, outdir, pyname):
+    """The core function.
 
-    except Exception as e:
-        sys.stderr.write('{0}\n'.format(e))
+    Args:
+        template: A template text for Bash script.
+        outdir: A directory shell scripts will be generated in.
+        pyname: A Python script.
+
+    Returns:
+        None.
+    """
+
+    name, ext = os.path.splitext(pyname)
+    if ext == '.py':
+        shname = os.path.join(outdir, name + '.sh')
+        with open(shname, mode='w', newline=NEWLINE_SEQUENCE) as fout:
+            fout.write(template.render(
+                PY_SCRIPT_NAME=os.path.realpath(pyname)))
+            fout.write(NEWLINE_SEQUENCE)
+            print('Processed {0}'.format(shname), file=sys.stderr)
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description=__doc__, version=__version__)
-    parser.add_argument(
-        '-d','--directory',
-        default='.',
-        help='target directory (default to current directory)')
-    args = parser.parse_args()
-    main(args)
+    main(configure().parse_args())
