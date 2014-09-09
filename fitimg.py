4293ebe9af4eb855429ed68ab80302031604528b
showa_yojyo <yojyo@hotmail.com>
2014-09-09 21:54:59 +0900

    (fitimg.py): run pylint.

--- fitimg.py
+++ fitimg.py
@@ -1,39 +1,75 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Copyright (c) 2012 プレハブ小屋 <yojyo@hotmail.com>
-All Rights Reserved.  NO WARRANTY.
+"""Resize large JPEG files in the current directory down to the screen
+resolution size with its aspect ratio preserved.
 
-Resizes larger JPEG files in the current directory to 1024x768 size
-(aspect ratio will be preserved).
+Examples:
+  You just call this script without arguments::
+
+    $ python fitimg.py
 """
 
 import sys
 import glob
+from argparse import ArgumentParser
 # PIL
 from PIL import Image
 
+__version__ = '1.0.1'
+
+# Constant values.
 PATTERN = '*.jpg'
-SCREEN_WIDTH = 1024
+SCREEN_WIDTH = 1366
 SCREEN_HEIGHT = 768
 
-def main():
+def configure():
+    """Parse the command line parameters.
+
+    Returns:
+        An instance of argparse.ArgumentParser that stores the command line
+        parameters.
+    """
+    parser = ArgumentParser(
+        description='Resize large JPEG files to the screen size')
+    parser.add_argument('--version', action='version', version=__version__)
+    return parser
+
+def main(args):
+    """The main function.
+
+    Args:
+        args: An instance of argparse.ArgumentParser parsed in the configure
+            function.
+
+    Returns:
+        None.
+    """
+
     for name in glob.iglob(PATTERN):
         process(name)
 
 def process(jpg):
-    try:
-        img = Image.open(jpg)
-        sz = img.size
-        if sz[0] <= SCREEN_WIDTH and sz[1] <= SCREEN_HEIGHT:
-            sys.stderr.write('Skipped: {0} ({1[0]}x{1[1]})\n'.format(jpg, sz))
-            return
+    """The main function.
 
-        newsz = (min(sz[0], SCREEN_WIDTH), min(sz[1], SCREEN_HEIGHT))
-        img.thumbnail(newsz, Image.ANTIALIAS)
-        img.save(jpg)
-        sys.stderr.write('Resized: {0} ({1[0]}x{1[1]})\n'.format(jpg, img.size))
-    except Exception as e:
-        sys.stderr.write('{0}\n'.format(e))
+    Args:
+        jpg: A path to JPEG file.
+
+    Returns:
+        None.
+    """
+
+    img = Image.open(jpg)
+    extent = img.size
+    if extent[0] <= SCREEN_WIDTH and extent[1] <= SCREEN_HEIGHT:
+        print('Skipped: {0} ({size[0]}x{size[1]})'.format(jpg, size=extent),
+              file=sys.stderr)
+        return
+
+    new_extent = (min(extent[0], SCREEN_WIDTH), min(extent[1], SCREEN_HEIGHT))
+    img.thumbnail(new_extent, Image.ANTIALIAS)
+    img.save(jpg)
+    print('Resized: {0} ({size[0]}x{size[1]})'.format(jpg, size=img.size),
+          file=sys.stderr)
 
 if __name__ == '__main__':
-    main()
+    main(configure().parse_args())
