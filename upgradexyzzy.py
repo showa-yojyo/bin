7b2391bc4de6f808c1a1d086a4ce71f4e5a25ed1
showa_yojyo <yojyo@hotmail.com>
2014-09-19 01:47:30 +0900

    (upgradexyzzy.py): import BeatifulSoup4 and rewrite this script.

--- upgradexyzzy.py
+++ upgradexyzzy.py
@@ -1,46 +1,99 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Copyright (c) 2012 プレハブ小屋 <yojyo@hotmail.com>
-All Rights Reserved.  NO WARRANTY.
+"""This script fetches the current Xyzzy archive from xyzzy-022.github.com
+and installs on your machine.
 
-最新版の Xyzzy をダウンロードして、所定のパスに解凍する。
+Example:
+   You run this script without arguments::
+
+   $ python upgradexyzzy.py
 """
 
-from BeautifulSoup import BeautifulSoup
+from argparse import ArgumentParser
+from bs4 import BeautifulSoup
 from distutils.dir_util import copy_tree
-
 import os
 import shutil
-import urllib.request, urllib.error, urllib.parse
+import sys
+import urllib.request
+import urllib.error
+import urllib.parse
 import zipfile
 
-XYZZY_GITHUB_URL = r'http://xyzzy-022.github.com/'
+__version__ = '1.1.0'
+
+XYZZY_GITHUB_URL = r'http://xyzzy-022.github.io/'
 
 # TODO: user settings
-XYZZY_DEST = r'D:/Program Files(x86)/xyzzy/'
+XYZZY_DEST = r'D:/Program Files/xyzzy/'
 WORK_DIR = r'D:/Temp/'
 
-def get_latest_xyzzy_url():
+def configure():
+    """Parse the command line parameters.
+
+    Returns:
+        An instance of argparse.ArgumentParser that stores the command line
+        parameters.
+    """
+
+    parser = ArgumentParser(description='Xyzzy Manager')
+    parser.add_argument('--version', action='version', version=__version__)
+
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help='a lot more information output')
+
+    return parser.parse_args()
+
+def get_xyzzy_url():
+    """Get the URL of the current Xyzzy archive (ZIP) file.
+
+    Returns:
+        The URL of xyzzy-X.Y.Z.zip.
+    """
+
     url = XYZZY_GITHUB_URL
     html = urllib.request.urlopen(url).read()
 
     # <div class="download">
-    # <a href="https://github.com/downloads/xyzzy-022/xyzzy/xyzzy-0.2.2.241.zip">
+    # <a href="/downloads/xyzzy-0.2.2.253.zip">
     # ...
+    # </a>
     # </div>
     soup = BeautifulSoup(html)
-    div = soup.findAll('div', attrs={'class':'download'})
-    a = div[0].find('a')
-    zipurl = a['href']
+    zipurl = soup.find('div', class_='download').find('a')['href']
 
-    # e.g. 'https://github.com/downloads/xyzzy-022/xyzzy/xyzzy-0.2.2.241.zip'
+    if zipurl.startswith('http://') or zipurl.startswith('https://'):
+        pass
+    elif zipurl.startswith('/'):
+        zipurl = urllib.parse.urljoin(XYZZY_GITHUB_URL, zipurl)
+
+    # e.g. 'http://xyzzy-022.github.io/xyzzy-022/xyzzy/xyzzy-0.2.2.253.zip'
     return zipurl
 
-def download_xyzzy(zipurl, workdir):
-    f = urllib.request.urlopen(zipurl)
-    print('Download: {0}: done.'.format(zipurl))
+def download_xyzzy(args, zipurl, workdir):
+    """Download the current Xyzzy archive (ZIP) file from the website.
 
-    # e.g. 'xyzzy-0.2.2.241.zip'
+    Args:
+        args: An instance of argparse.ArgumentParser parsed in the configure
+            function.
+        zipurl: URL of xyzzy-X.Y.Z.zip.
+        workdir: Path to user's working directory.
+
+    Returns:
+        The path of xyzzy-X.Y.Z.zip downloaded.
+    """
+
+    if args.verbose:
+        print('Downloading: {0}...'.format(zipurl), file=sys.stderr)
+
+    localfile = urllib.request.urlopen(zipurl)
+
+    if args.verbose:
+        print('Done.', file=sys.stderr)
+
+    # e.g. 'xyzzy-0.2.2.253.zip'
     zipname = os.path.basename(zipurl)
     zippath = os.path.join(workdir, zipname)
     if os.path.exists(zippath):
@@ -48,36 +101,62 @@ def download_xyzzy(zipurl, workdir):
 
     # Save the zip file to workdir
     with open(zippath, 'wb') as fout:
-        fout.write(f.read())
+        fout.write(localfile.read())
 
     return zippath
 
 def overwrite_xyzzy(workdir, destdir):
+    """Overwrite with new Xyzzy packages.
+
+    Args:
+        workdir: Path to user's working directory.
+        destdir: Xyzzy's directory, i.e. $XYZZYHOME.
+
+    Returns:
+        The path of xyzzy-X.Y.Z.zip downloaded.
+    """
+
     xyzzysrc = os.path.join(workdir, 'xyzzy')
     copy_tree(xyzzysrc, destdir)
     return xyzzysrc, destdir
 
-def main():
-    zipurl = get_latest_xyzzy_url()
+def main(args):
+    """The main function.
 
-    zippath = download_xyzzy(zipurl, WORK_DIR)
-    print('Saved to: {0}'.format(zippath))
+    Args:
+        args: An instance of argparse.ArgumentParser parsed in the configure
+            function.
+
+    Returns:
+        None.
+    """
+    zipurl = get_xyzzy_url()
+    if args.verbose:
+        print('URL: {}'.format(zipurl), file=sys.stderr)
+
+    zippath = download_xyzzy(args, zipurl, WORK_DIR)
+    if args.verbose:
+        print('Saved to: {}'.format(zippath), file=sys.stderr)
 
     # Extract the archive and overwrite to XYZZY_DEST.
-    arch = zipfile.ZipFile(zippath, 'r')
-    arch.extractall(WORK_DIR)
-    arch.close()
+    with zipfile.ZipFile(zippath, 'r') as arch:
+        arch.extractall(WORK_DIR)
 
     xyzzysrc, xyzzyhome = overwrite_xyzzy(WORK_DIR, XYZZY_DEST)
-    print('Copied {0} to {1}...'.format(xyzzysrc, xyzzyhome))
+    if args.verbose:
+        print('Copied {0} to {1}...'.format(xyzzysrc, xyzzyhome))
 
     # Clean up temporary files.
-    print('Delete {0}'.format(zippath))
+    if args.verbose:
+        print('Delete {}...'.format(zippath), file=sys.stderr)
     os.remove(zippath)
-    print('Delete {0}'.format(xyzzysrc))
+
+    if args.verbose:
+        print('Delete {}...'.format(xyzzysrc), file=sys.stderr)
     shutil.rmtree(xyzzysrc)
 
-    print('Finished.')
+    if args.verbose:
+        print('Finished.', file=sys.stderr)
 
 if __name__ == '__main__':
-    main()
+    main(configure())
