508b9b9495010c80ba104c82679fcc0f0c4dc272
showa_yojyo <yojyo@hotmail.com>
2015-12-02 23:08:49 +0900

    (twstream) Almost complete streaming subcommands.

--- twstream.py
+++ twstream.py
@@ -1,50 +1,221 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""twstream.py
+"""twstream.py: Twitter Streaming API Utility
+
+Usage:
+  twstream.py [common-options] <command> [command-options]
 """
 
 from secret import twitter_stream
+from twitter import TwitterHTTPError
 from twitter.stream import Timeout, HeartbeatTimeout, Hangup
+from common_twitter import AbstractTwitterManager
+from twstreamcommands import make_commands
+from argparse import ArgumentParser
 from pprint import pprint
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
-def main():
-    # These arguments are optional:
-    stream_args = dict(
-        timeout=60,
-        block=False,
-        heartbeat_timeout=90)
+class TwitterStreamManager(AbstractTwitterManager):
+    """Twitter Streaming API Utility"""
 
-    query_args = dict(
-        language='ja',
-        track='bot')
+    def __init__(self):
+        super().__init__('twstream', make_commands(self))
 
-    csv_header = (
-        'id',
-        'created_at',
-        'user[screen_name]',
-        'user[name]',
-        #'source',
-        'text',)
-    csv_format = '|'.join(('{' + i + '}' for i in csv_header))
+    def make_parser(self):
+        """Create the command line parser.
 
-    stream = twitter_stream(**stream_args)
-    for tweet in stream.statuses.filter(**query_args):
-        if not tweet:
-            continue
+        Returns:
+            An instance of argparse.ArgumentParser that will store the command line
+            parameters.
+        """
 
-        if tweet in (Timeout, HeartbeatTimeout, Hangup):
-            print("{}".format(tweet))
-            break
+        parser = ArgumentParser(description='Twitter Streaming API Utility')
+        parser.add_argument('--version', action='version', version=__version__)
 
-        if 'text' in tweet:
-            #pprint(tweet)
-            line = csv_format.format(**tweet)
-            print(line.replace('\r', '').replace('\n', '\\n'))
-            continue
+        parser.add_argument(
+            '-o', '--timeout',
+            default=60,
+            help='timeout for the stream (seconds)')
+        parser.add_argument(
+            '-H', '--heartbeat',
+            default=90,
+            help='set heartbeat timeout (seconds)')
+        parser.add_argument(
+            '-b', '--block',
+            action='store_true',
+            default=True,
+            help='set stream to blocking')
 
-        print("Unknown {}".format(tweet))
+        group = parser.add_argument_group(title='Streaming API arguments')
+        group.add_argument(
+            '--delimited',
+            metavar='length',
+            help='do not use this option')
+        group.add_argument(
+            '--stall-warnings',
+            action='store_true',
+            default=False,
+            dest='stall_warnings',
+            help='enable periodic warning messages if the client is in danger of being disconnected')
+        group.add_argument(
+            '--filter-level',
+            dest='filter_level',
+            choices=('none', 'low', 'medium',),
+            help='the minimum value of the filter level')
+        group.add_argument(
+            '--language',
+            help='a comma-separated list of BCP 47 language identifiers')
+
+        return parser
+
+    def execute(self):
+        """Execute the specified command."""
+
+        try:
+            self.args.func()
+        except TwitterHTTPError as e:
+            self.logger.error('{}'.format(e))
+            raise
+
+    def request_statuses_sample(self):
+        """Request GET statuses/sample for Twitter."""
+
+        tw = self._setup_stream()
+        query_args = {k:args[k] for k in (
+            'delimited', 'stall_warnings',)
+                if k in args}
+
+        g = tw.statuses.sample(**query_args)
+        self._output_statuses(g)
+
+    def request_statuses_filter(self):
+        """Request POST statuses/filter for Twitter."""
+
+        tw = self._setup_stream()
+        query_args = {k:args[k] for k in (
+            'follow', 'track', 'locations',
+            'delimited', 'stall_warnings',)
+                if k in args}
+
+        g = tw.statuses.filter(**query_args)
+        self._output_statuses(g)
+
+    def request_user(self):
+        """Request GET user for Twitter."""
+
+        tw = self._setup_stream(domain='userstream.twitter.com')
+        query_args = {k:args[k] for k in (
+            'delimited', 'stall_warnings',
+            'with', 'replies',
+            'track', 'locations',)
+                if k in args}
+
+        for msg in tw.user(**query_args):
+            if not tweet:
+                continue
+
+            if tweet in (Timeout, HeartbeatTimeout, Hangup):
+                print("{}".format(tweet))
+                break
+
+            # Friends lists.
+            if 'friends' in msg or 'friends_str' in msg:
+                pprint(msg)
+                continue
+
+            # Direct messages.
+            if 'recipient' in msg and 'sender' in msg:
+                pprint(msg)
+                continue
+
+            # Events
+            if 'event' in msg:
+                pprint(msg)
+                continue
+
+            # Warnings
+            if 'warning' in msg:
+                pprint(msg)
+                continue
+
+            # Unknown
+            print("Unknown {}".format(msg))
+
+    def request_site(self):
+        """Request GET site for Twitter."""
+
+        tw = self._setup_stream(domain='sitestream.twitter.com')
+        query_args = {k:args[k] for k in (
+            'follow',
+            'delimited', 'stall_warnings',
+            'with', 'replies',)
+                if k in args}
+
+        for msg in tw.site(**query_args):
+            pprint(msg)
+
+    def request_statuses_firehose(self):
+        """Request GET statuses/firehose for Twitter."""
+
+        tw = self._setup_stream()
+        query_args = {k:args[k] for k in (
+            'count',
+            'delimited', 'stall_warnings',)
+                if k in args}
+
+        g = tw.statuses.firehose(**query_args)
+        self._output_statuses(g)
+
+    def _setup_stream(self, **kwargs):
+        """x"""
+
+        args = vars(self.args)
+        stream_args = {k:args[k] for k in (
+            'timeout',
+            'block',
+            'heartbeat_timeout')
+                if k in args}
+        return twitter_stream(**kwargs, **stream_args)
+
+    def _output_statuses(self, gen):
+        """x"""
+
+        csv_header = (
+            'id',
+            'created_at',
+            'user[screen_name]',
+            'user[name]',
+            #'source',
+            'text',)
+        csv_format = '|'.join(('{' + i + '}' for i in csv_header))
+
+        for tweet in gen:
+            if not tweet:
+                continue
+
+            if tweet in (Timeout, HeartbeatTimeout, Hangup):
+                print("{}".format(tweet))
+                break
+
+            if 'text' in tweet:
+                #pprint(tweet)
+                line = csv_format.format(**tweet)
+                print(line.replace('\r', '').replace('\n', '\\n'))
+                continue
+
+            print("Unknown {}".format(tweet))
+
+def main(command_line=None):
+    """The main function.
+
+    Args:
+        command_line: Raw command line arguments.
+    """
+
+    mgr = TwitterStreamManager()
+    mgr.setup(command_line)
+    mgr.execute()
 
 if __name__ == '__main__':
     main()
---
+++ twstreamcommands.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""twstreamcommands.py:
+"""
+__version__ = '1.1.0'
+
+from common_twitter import AbstractTwitterCommand
+from argparse import ArgumentParser
+
+# Public API
+# GET statuses/sample
+COMMAND_STATUSES_SAMPLE = ('statuses-sample', 'sample')
+
+# POST statuses/filter
+COMMAND_STATUSES_FILTER = ('statuses-filter', 'filter')
+
+# User Streams (domain='userstream.twitter.com')
+# GET user
+COMMAND_USER = ('user',)
+
+# Site Streams (domain='sitestream.twitter.com')
+# GET site
+COMMAND_SITE = ('site',)
+
+# GET c/:stream_id/info
+# ?
+
+# Firehose
+# GET statuses/firehose
+COMMAND_STATUSES_FIREHOSE = ('statuses-firehose', 'firehose', 'fire')
+
+class CommandStatusesSample(AbstractTwitterCommand):
+    """Print a small random sample of all public statuses."""
+
+    def create_parser(self, subparsers):
+        parser = subparsers.add_parser(
+            COMMAND_STATUSES_SAMPLE[0],
+            aliases=COMMAND_STATUSES_SAMPLE[1:],
+            help='print a small random sample of all public statuses')
+        return parser
+
+    def __call__(self):
+        self.manager.request_statuses_sample()
+
+class CommandStatusesFilter(AbstractTwitterCommand):
+    """Print public statuses that match one or more filter predicates."""
+
+    def create_parser(self, subparsers):
+        parser = subparsers.add_parser(
+            COMMAND_STATUSES_FILTER[0],
+            aliases=COMMAND_STATUSES_FILTER[1:],
+            parents=[parser_follow(), parser_track_and_locations(),],
+            help='print public statuses that match one or more filter predicates')
+        return parser
+
+    def __call__(self):
+        self.manager.request_statuses_filter()
+
+class CommandUser(AbstractTwitterCommand):
+    """Stream messages for a single user."""
+
+    def create_parser(self, subparsers):
+        parser = subparsers.add_parser(
+            COMMAND_USER[0],
+            #aliases=COMMAND_USER[1:],
+            parents=[parser_track_and_locations(), parser_with_and_replies()],
+            help='stream messages for a single user')
+        return parser
+
+    def __call__(self):
+        self.manager.request_user()
+
+class CommandSite(AbstractTwitterCommand):
+    """Stream messages for a set of users."""
+
+    def create_parser(self, subparsers):
+        parser = subparsers.add_parser(
+            COMMAND_SITE[0],
+            #aliases=COMMAND_SITE[1:],
+            parents=[parser_follow(), parser_with_and_replies()],
+            help='stream messages for a set of users')
+        return parser
+
+    def __call__(self):
+        self.manager.request_site()
+
+class CommandStatusesFirehose(AbstractTwitterCommand):
+    """Print all public statuses."""
+
+    def create_parser(self, subparsers):
+        parser = subparsers.add_parser(
+            COMMAND_STATUSES_FIREHOSE[0],
+            aliases=COMMAND_STATUSES_FIREHOSE[1:],
+            help='print all public statuses')
+        return parser
+
+    def __call__(self):
+        self.manager.request_statuses_firehose()
+
+def _parser_track_and_locations():
+    parser = None
+    def inner():
+        nonlocal parser
+        if parser:
+            return parser
+
+        parser = ArgumentParser(add_help=False)
+        parser.add_argument(
+            '--track',
+            metavar='CSV',
+            help='A comma-separated list of phrases to filter tweets')
+        parser.add_argument(
+            '--locations',
+            metavar='CSV',
+            help='A comma-separated list of coordinates specifying a set of bounding boxes to filter tweets')
+
+        return parser
+    return inner
+parser_track_and_locations = _parser_track_and_locations()
+
+def _parser_follow():
+    parser = None
+    def inner():
+        nonlocal parser
+        if parser:
+            return parser
+
+        parser = ArgumentParser(add_help=False)
+        parser.add_argument(
+            '--follow',
+            metavar='CSV',
+            help='A comma-separated list of user IDs to filter tweets')
+
+        return parser
+    return inner
+parser_follow = _parser_follow()
+
+def _parser_with_and_replies():
+    parser = None
+    def inner():
+        nonlocal parser
+        if parser:
+            return parser
+
+        parser = ArgumentParser(add_help=False)
+        parser.add_argument(
+            '--with',
+            choices=('user', 'followings',),
+            help='the types of messages')
+        parser.add_argument(
+            '--replies',
+            choices=('all',),
+            help='show all replies')
+
+        return parser
+    return inner
+parser_with_and_replies = _parser_with_and_replies()
+
+_command_classes = tuple(v for k, v in locals().items() if k.startswith('Command'))
+
+def make_commands(manager):
+    """Prototype"""
+    return [cmd_t(manager) for cmd_t in _command_classes]
