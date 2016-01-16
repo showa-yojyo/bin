9788df64d4dc6174a7dfef1217721052f4d363d5
showa_yojyo <yojyo@hotmail.com>
2016-01-16 23:21:07 +0900

    (mjstat.translator) Implement output in Japanese.

--- mjscore.py
+++ mjscore.py
@@ -5,6 +5,7 @@
 Usage:
   mjscore.py [--help] [--version]
   mjscore.py [--today] [--verbose] [--file FILE]
+    [-l | --language <langspec>]
     [-T | --target <playerspec>]
 """
 
@@ -14,6 +15,7 @@ from docutils.statemachine import StateMachine
 from mjstat import players_default
 from mjstat.states import (initial_state, state_classes)
 from mjstat.stat import evaluate
+from mjstat.translator import output
 import sys
 
 __version__ = '0.0.0'
@@ -38,6 +40,11 @@ def configure():
         '--verbose',
         action='store_true',
         help='enable verbose mode')
+
+    parser.add_argument(
+        '-l', '--language',
+        default='en',
+        help='set the language (ISO 639-1) for output')
 
     # TODO: (priority: high) reference period as a parameter
     parser.add_argument(
@@ -68,69 +75,23 @@ def main():
 
     # TODO: (priority: low) Define score model.
     context = {}
-    results = state_machine.run(input_lines, context=context)
+    state_machine.run(input_lines, context=context)
 
     if args.verbose:
-        print(results)
         dump(context, sys.stdout, ensure_ascii=False, indent=4, sort_keys=True)
         sys.stdout.write("\n")
 
     state_machine.unlink()
 
+    lang = args.language
     target_player = args.target_player
     if target_player == 'all':
         for target_player in players_default:
             evaluate(context, target_player)
-            output(context, target_player)
+            output(context, target_player, lang)
     else:
         evaluate(context, target_player)
-        output(context, target_player)
-
-def output(game_data, target_player):
-    """Show the statistics of the target player."""
-
-    print('Date:', game_data['date'])
-
-    player_data = game_data['player_stats'][target_player]
-    target_games = player_data['games']
-    if not target_games:
-        print('No data.')
-        return
-
-    print('Reference period: <{}> - <{}>'.format(
-        target_games[0]['started_at'],
-        target_games[-1]['finished_at']))
-
-    print('Player data')
-    print('  Name:', target_player)
-    print('  Number of games:', len(target_games))
-    print('  Number of hands:', player_data['count_hands'])
-
-    print('Placings')
-    print('  [1st, 2nd, 3rd, 4th]:', player_data['placing_distr'])
-    print('  First placing rate: {:.2f}%'.format(player_data['first_placing_rate'] * 100))
-    print('  Last placing rate: {:.2f}%'.format(player_data['last_placing_rate'] * 100))
-    print('  Mean: {:.2f}th'.format(player_data['mean_placing']))
-
-    print('Winnings')
-    print('  Number of winning:', player_data['winning_count'])
-    print('  Rate: {:.2f}%'.format(player_data['winning_rate'] * 100))
-    print('  Mean: {:.2f}pts.'.format(player_data['winning_mean']))
-    print('  Mean winning han: {:.2f}'.format(player_data['winning_mean_han']))
-    print('  Mean winning turns: {:.2f}'.format(player_data['winning_mean_turns']))
-
-    print('Losings on discard')
-    print('  Number of LOD:', player_data['lod_count'])
-    print('  Rate: {:.2f}%'.format(player_data['lod_rate'] * 100))
-    print('  Mean: {:.2f}pts.'.format(player_data['lod_mean']))
-
-    print('Riichi data')
-    print('  Total:', player_data['riichi_count'])
-    print('  Rate: {:.2f}%'.format(player_data['riichi_rate'] * 100))
-
-    print('Melding data')
-    print('  Total:', player_data['melding_count'])
-    print('  Rate: {:.2f}%'.format(player_data['melding_rate'] * 100))
+        output(context, target_player, lang)
 
 if __name__ == '__main__':
     main()
---
+++ mjstat/translator.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""translator.py: Translate text to output in specified language.
+"""
+
+template_en = \
+'''Date {date}
+Reference period: {started_at} - {finished_at}
+Player data
+  Name: {name}
+  Number of games: {count_games} games
+  Number of hands: {count_hands} hands
+Placing data
+  [1st, 2nd, 3rd, 4th]: {placing_distr}
+  1st-place prob.: {first_placing_rate:.2%}
+  4th-place prob.: {last_placing_rate:.2%}
+  Mean place: {mean_placing:.2f}th
+Wins
+  Winning percentage: {winning_rate:.2%} ({winning_count}/{count_hands})
+  Mean points: {winning_mean:.2f} pts.
+  Mean han: {winning_mean_han:.2f} han
+  Mean turns: {winning_mean_turns:.2f} turns
+Losses on deal-in
+  Deal-in percentage: {lod_rate:.2%} ({lod_count}/{count_hands})
+  Mean points: {lod_mean:.2f} pts.
+Riichi data
+  Riichi prob.: {riichi_rate:.2%} ({riichi_count}/{count_hands})
+Melding data
+  Melding prob.: {melding_rate:.2%} ({melding_count}/{count_hands})'''
+
+template_ja = \
+'''集計日 {date}
+集計期間 {started_at} - {finished_at}
+プレイヤーデータ
+  名前 {name}
+  ゲーム数 {count_games} 試合
+  局数 {count_hands} 局
+着順データ
+  [1st, 2nd, 3rd, 4th]: {placing_distr}
+  トップ率 {first_placing_rate:.2%}
+  ラス率 {last_placing_rate:.2%}
+  平均着順 {mean_placing:.2f} 着
+アガリデータ
+  アガリ率 {winning_rate:.2%} ({winning_count}/{count_hands})
+  平均得点 {winning_mean:.2f} 点
+  平均アガリ飜 {winning_mean_han:.2f}
+  平均アガリ巡目 {winning_mean_turns:.2f}
+放銃データ
+  放銃率 {lod_rate:.2%} ({lod_count}/{count_hands})
+  平均失点 {lod_mean:.2f} 点
+リーチデータ
+  平均使用率 {riichi_rate:.2%} ({riichi_count}/{count_hands})
+鳴きデータ
+  平均鳴き回数 {melding_rate:.2%} ({melding_count}/{count_hands})'''
+
+template_map = {
+    'en':template_en,
+    'ja':template_ja,}
+
+def output(game_data, target_player, language='en'):
+    """Show the statistics of the target player."""
+
+    player_data = game_data['player_stats'][target_player]
+    target_games = player_data['games']
+    if not target_games:
+        print('NO DATA')
+        return
+
+    output_template = template_map.get(language, template_en)
+
+    print(output_template.format(
+        date=game_data['date'],
+        count_games=len(target_games),
+        started_at=target_games[0]['started_at'],
+        finished_at=target_games[-1]['finished_at'],
+        **player_data))
