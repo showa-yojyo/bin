6727bb98c91f1185042d08f3ff2a4c5ef625cae4
showa_yojyo <yojyo@hotmail.com>
2016-02-25 21:57:08 +0900

    Use importlib.import_module instead of built-in __import__.

--- mjstat/languages/__init__.py
+++ mjstat/languages/__init__.py
@@ -2,25 +2,24 @@
 """__init__.py: Language-dependent features.
 """
 
+from importlib import import_module
+
 module_cache = {}
 
 def get_language(lang_code):
     """Return module with language localizations.
 
-    This is a poor copy of the language framework of Docutils.
+    This is a revamped version of function docutils.languages.get_language.
     """
 
     if lang_code in module_cache:
         return module_cache[lang_code]
 
-    for i in (1, 0):
-        try:
-            module = __import__(lang_code, globals(), locals(), level=i)
-            break
-        except ImportError:
-            continue
-    else:
-        module = __import__('en', globals(), locals(), level=1)
+    try:
+        module = import_module('.' + lang_code, __name__)
+    except ImportError:
+        from . import en
+        module = en
 
     module_cache[lang_code] = module
     return module
