9bd921d0614e1ae7e66bfc44f892463f796509a1
showa_yojyo <yojyo@hotmail.com>
2016-08-18 22:48:45 +0900

    (glshowenv.py) Add a script that shows values of GL_VENDER, GL_RENDERER, and GL_VERSION.

---
+++ glshowenv.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""glshowenv.py: Show the current OpenGL connection strings.
+
+Usage:
+  glshowenv.py [GLUT_OPTIONS]
+"""
+
+import sys
+import OpenGL.GL as GL
+import OpenGL.GLUT as GLUT
+
+def main():
+    """Show the current OpenGL connection strings."""
+
+    GLUT.glutInit(sys.argv)
+    GLUT.glutInitContextVersion(3, 3)
+    GLUT.glutInitContextProfile(GLUT.GLUT_CORE_PROFILE)
+    GLUT.glutInitDisplayMode(
+        GLUT.GLUT_DOUBLE | GLUT.GLUT_RGBA | GLUT.GLUT_DEPTH)
+    win = GLUT.glutCreateWindow(b'NO TITLE')
+
+    aspects = {'Vendor':GL.GL_VENDOR,
+               'Renderer':GL.GL_RENDERER,
+               'Version':GL.GL_VERSION,}
+    if GL.glCreateShader:
+        aspects['GLSL'] = GL.GL_SHADING_LANGUAGE_VERSION
+
+    print('\n'.join('{}: {}'.format(key, GL.glGetString(val).decode())
+                    for key, val in aspects.items()))
+
+    GLUT.glutDestroyWindow(win)
+
+if __name__ == '__main__':
+    main()
