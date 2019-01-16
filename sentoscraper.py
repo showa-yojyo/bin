544f937efa75e9a3d974e459ff9ae102627632e2
showa_yojyo <yojyo@hotmail.com>
2019-01-16 16:27:49 +0900

    (sentoscraper) Sligtly improve text processing

--- sentoscraper.py
+++ sentoscraper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import asyncio
+import re
 import sys
 from argparse import ArgumentParser
 from collections import namedtuple
@@ -8,68 +9,60 @@ from urllib import request
 
 from bs4 import BeautifulSoup
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 TOP = 'http://www.1010.or.jp/map/item/'
 URLS = (
-    'item-cnt-251', # 興和浴場
-    'item-cnt-253', # 湯パーク松島
-    'item-cnt-254', # 千代の湯
-    'item-cnt-255', # 大黒湯（休業中）
-    'item-cnt-256', # 金町湯
-    'item-cnt-257', # 栄湯
-    'item-cnt-258', # 富士の湯
-    'item-cnt-259', # ゆートピア２１
-    'item-cnt-260', # 第一日立湯
-    'item-cnt-261', # 日の出湯
-    'item-cnt-262', # 日の出湯
-    'item-cnt-266', # 富士の湯
-    'item-cnt-267', # 寿湯
-    'item-cnt-268', # 吉の湯
-    'item-cnt-269', # 美吉湯
-    'item-cnt-270', # さつき湯
-    'item-cnt-271', # 草津湯
-    'item-cnt-272', # 久の湯
-    'item-cnt-273', # 第四富士の湯
-    'item-cnt-274', # 橘湯
-    'item-cnt-237', # 竹の湯
-    'item-cnt-240', # アクアガーデン栄湯
-    'item-cnt-241', # 寿湯
-    'item-cnt-242', # 富の湯
-    'item-cnt-244', # 成弘湯
-    'item-cnt-245', # アクアドルフィンランド
-    'item-cnt-246', # 喜久の湯
-    'item-cnt-247', # 末広湯
-#    'item-cnt-479', # 八幡湯
-#    'item-cnt-480', # 駒の湯
-#    'item-cnt-481', # 栄湯
-#    'item-cnt-482', # 弘善湯
-#    'item-cnt-483', # ノザワランド（休業中）
-#    'item-cnt-485', # 天狗湯
-#    'item-cnt-486', # 富士見湯
-#    'item-cnt-487', # 千代の湯
-#    'item-cnt-488', # 世田谷温泉 四季の湯
-#    'item-cnt-489', # 鶴の湯
-#    'item-cnt-490', # 石川湯
-#    'item-cnt-491', # 宇田川湯
-#    'item-cnt-492', # 松原湯
-#    'item-cnt-493', # 月見湯温泉
-#    'item-cnt-494', # 山崎湯
-#    'item-cnt-495', # 新寿湯
-#    'item-cnt-497', # 北沢湯
-#    'item-cnt-498', # 松の湯（休業中）
-#    'item-cnt-499', # 増穂湯
-#    'item-cnt-500', # 丸正浴場
-#    'item-cnt-501', # えの木湯（休業中）
-#    'item-cnt-502', # 湯パークレビランド
-#    'item-cnt-503', # そしがや温泉２１
-#    'item-cnt-504', # 塩の湯（休業中）
-#    'item-cnt-505', # 給田湯
-#    'item-cnt-506', # つばめ湯（休業中）
-#    'item-cnt-507', # 松の湯
-#    'item-cnt-509', # 藤の湯
-#    'item-cnt-510', # 栗の湯（休業中）
-#    'item-cnt-511', # 栄湯
+    # 'item-cnt-344', # 改良湯
+    # 'item-cnt-345', # 宝来湯
+    # 'item-cnt-346', # 広尾湯
+    # 'item-cnt-347', # さかえ湯
+    # 'item-cnt-350', # 渋谷笹塚温泉 栄湯
+    # 'item-cnt-351', # 観音湯
+    # 'item-cnt-353', # 第二かねき湯
+    # 'item-cnt-354', # 羽衣湯
+    # 'item-cnt-355', # 八幡湯
+    # 'item-cnt-356', # 仙石湯
+    # 'item-cnt-358', # 天神湯
+    # 'item-cnt-359', # 健康浴泉
+    # 'item-cnt-360', # アクア東中野
+    # 'item-cnt-361', # 松本湯
+    # 'item-cnt-362', # 千代の湯
+    # 'item-cnt-363', # 照の湯
+    # 'item-cnt-364', # 高砂湯
+    # 'item-cnt-365', # 昭和浴場
+    # 'item-cnt-366', # クラブ湯
+    # 'item-cnt-367', # 大黒湯
+    # 'item-cnt-369', # 清春湯
+    # 'item-cnt-370', # 月の湯
+    # 'item-cnt-371', # たからゆ
+    # 'item-cnt-372', # 昭和湯
+    # 'item-cnt-373', # 上越泉
+    # 'item-cnt-374', # 大和湯
+    # 'item-cnt-375', # 中野寿湯温泉
+    # 'item-cnt-376', # 新越泉
+    # 'item-cnt-378', # 一の湯
+    # 'item-cnt-379', # 江古田湯
+    'item-cnt-380', # 弁天湯
+    'item-cnt-381', # 香藤湯
+    'item-cnt-382', # なみのゆ
+    'item-cnt-383', # 小杉湯
+    'item-cnt-384', # 玉の湯
+    'item-cnt-385', # 天徳泉
+    'item-cnt-387', # 杉並湯
+    'item-cnt-388', # ゆ家和ごころ 吉の湯
+    'item-cnt-390', # 桜湯
+    'item-cnt-391', # 藤乃湯
+    'item-cnt-392', # 第二宝湯
+    'item-cnt-393', # 井草湯
+    'item-cnt-394', # 亀の湯
+    'item-cnt-395', # 秀の湯
+    'item-cnt-396', # 文化湯
+    'item-cnt-398', # 天狗湯
+    'item-cnt-399', # ＧＯＫＵＲＡＫＵＹＡ
+    'item-cnt-400', # 湯の楽代田橋
+    'item-cnt-401', # 大黒湯
+    'item-cnt-402', # 大和湯
     )
 
 class Sento(namedtuple('Sento',
@@ -92,6 +85,7 @@ def parse_args(args):
 
     parser.add_argument(
         '-s', '--semaphore',
+        type=int,
         default=3,
         help='the count of the semaphore'
     )
@@ -107,7 +101,9 @@ async def fetch(args):
             return await scrape(url, localpath)
 
     return await asyncio.wait(
-        [scrape_with_semaphore(TOP + url, '{}.html'.format(url)) for url in URLS])
+        [scrape_with_semaphore(TOP + url, f'{url}.html') for url in URLS])
+
+TABLE = str.maketrans('０１２３４５６７８９：−', '0123456789:-')
 
 async def scrape(url, localpath):
     """TODO: docstring"""
@@ -121,14 +117,19 @@ async def scrape(url, localpath):
             fout.write(data)
 
     def sanitize(text):
-        return text.replace('\t', ' ').strip()
+        return re.sub(r'\s+', '', text.strip()).translate(TABLE)
 
-    bs = BeautifulSoup(data, "html.parser")
+    def process_address(text):
+        """Remove area code from given address"""
+        return sanitize(text[9:])
+
+    bs = BeautifulSoup(data, "lxml")
+
     sento = Sento(
         id=localpath,
         name=bs.find('h2').text,
         has_laundry=0 if bs.find('a', string="コインランドリー") is None else 1,
-        address=sanitize(bs.find(string="住所").find_next('td').text),
+        address=process_address(bs.find(string="住所").find_next('td').text),
         access=sanitize(bs.find(string="アクセス").find_next('td').text),
         holidays=sanitize(bs.find(string="休日").find_next('td').text),
         office_hours=sanitize(bs.find(string="営業時間").find_next('td').text))
