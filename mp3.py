a9c653948414f8bb5bf374fa59b4064e2de891b2
showa_yojyo <yojyo@hotmail.com>
2019-07-24 16:16:32 +0900

    (mp3.py) Accept multiple URLs

--- mp3.py
+++ mp3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 """
-mp3 downloader protptype
+mp3 downloader prototype
 
 Usage:
-$ mp3.py url
+$ mp3.py [OPTION] YOUTUBE_WATCH_URL...
 Example:
-$ mp3.py https://www.youtube.com/watch?v=xxxxxxxxxx
+$ mp3.py --save https://www.youtube.com/watch?v=xxxxxxxxxx
 """
 
 from argparse import ArgumentParser
@@ -24,17 +24,22 @@ def parse_args(args):
 
     parser = ArgumentParser(description='mp3 downloader protptype')
     parser.add_argument(
+        '-d', '--destination',
+        dest='dest_dir',
+        metavar='DEST',
+        default='.',
+        help='dirctory in which files are saved')
+    parser.add_argument(
         '-s', '--save',
         action='store_true',
         help='write downloaded media to disk')
-
     parser.add_argument(
         '-v', '--verbose',
         action='store_true',
         help='explain what is being done')
-
     parser.add_argument(
         'url',
+        nargs='*',
         help='URL from which to extract mp3 files')
 
     parser.add_argument('--version', action='version', version=__version__)
@@ -46,22 +51,36 @@ def download(url):
     Without writing any media to disk.
 
     :param str url:
-       A YouTube watch URL.
+        A YouTube watch URL.
     :returns:
-       A stream of mp4?
+        A stream of mp4?
     """
     tube = YouTube(url)
     return tube.streams.filter(only_audio=True, file_extension='mp4').first()
 
+def save(media, dest_dir):
+    """Save media to disk
+
+    :param Stream media:
+        Media stream to be saved.
+    :param str dest_dir:
+        Output path for writing media file.
+    :rtype str:
+    :returns:
+        Output file path.
+    """
+
+    return media.download(output_path=dest_dir)
+
 def run(args):
     """main function"""
 
-    media = download(args.url)
-    if args.verbose:
-        print(media.default_filename)
-
-    if args.save:
-        media.download()
+    for i in args.url:
+        media = download(i)
+        if args.verbose:
+            print(media.default_filename)
+        if args.save:
+            save(media, args.dest_dir)
 
 def main(args=sys.argv[1:]):
     """main function"""
