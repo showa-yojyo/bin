bf730a93ba03647cf6a2f21e05278aa1a05eda5f
showa_yojyo <yojyo@hotmail.com>
2019-11-16 12:31:26 +0900

    (wifiota) stable version

--- mjnet.py
+++ mjnet.py
@@ -5,29 +5,49 @@
 
 import sys
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 MJ_NET_URL = 'https://www.sega-mj.net/mjac_p'
 MJ_NET_URL_SIGN_IN = MJ_NET_URL + '/mjlogin/login.jsp'
 MJ_NET_URL_SIGN_OUT = MJ_NET_URL + '/FwdPage?page=logout'
+MJ_NET_URL_TOP_PAGE = MJ_NET_URL + '/FwdPage?page=top'
+MJ_NET_URL_PLAYER_DATA_PAGE = MJ_NET_URL + '/FwdPage?page=playdata&i=0'
 
 def main():
     """The function invoked when the program is executed"""
 
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("Usage: mjnet.py USER_NAME PASSWORD", file=sys.stderr)
         sys.exit(2)
 
     driver = webdriver.Edge()
-    sign_in(driver, sys.argv[0], sys.argv[1])
+    sign_in(driver, sys.argv[1], sys.argv[2])
     try:
-        #go_to_top(driver) # FIXME
+        go_to_top(driver)
         go_to_play_data(driver)
         go_to_tompu_pro(driver)
         go_to_daily_record(driver)
         print(get_daily_record(driver))
-    finally:
         sign_out(driver)
+    finally:
         driver.close()
+
+def pause(func):
+    """debugging"""
+    def wrapper(driver):
+        print(func.__name__)
+
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.END)
+        actions.perform()
+
+        #breakpoint()
+        return func(driver)
+    return wrapper
 
 def sign_in(driver, user_name, password):
     """Sign in MJ.NET"""
@@ -41,27 +61,36 @@ def sign_out(driver):
     """Sign out of MJ.NET"""
     driver.get(MJ_NET_URL_SIGN_OUT)
 
+@pause
 def go_to_top(driver):
-    """FIXME"""
-    driver.find_element_by_css_selector('a[accesskey="0"]').click()
+    """Move to the top page"""
+    driver.get(MJ_NET_URL_TOP_PAGE)
 
+@pause
 def go_to_play_data(driver):
     """Move to プレイデータ"""
-    driver.find_element_by_class_name('player-status-basic').click()
+    driver.get(MJ_NET_URL_PLAYER_DATA_PAGE)
 
+@pause
 def go_to_tompu_pro(driver):
     """Move to プロ卓東風戦"""
     links = driver.find_elements_by_link_text('東風戦')
-    assert len(links) > 2
+    assert len(links) >= 2
     links[1].click()
 
+@pause
 def go_to_daily_record(driver):
     """Move to デイリー戦績"""
-    driver.find_elements_by_link_text('デイリー戦績').click()
 
+    WebDriverWait(driver, 60).until(
+        expected_conditions.presence_of_element_located(
+            (By.LINK_TEXT, 'デイリー戦績')))
+    driver.find_element_by_link_text('デイリー戦績').click()
+
+@pause
 def get_daily_record(driver):
     """Return the daily record"""
-    return driver.find_element_by_css_selector('div.common-wrap')
+    return driver.find_element_by_css_selector('div.common-wrap').text
 
 if __name__ == "__main__":
     main()
