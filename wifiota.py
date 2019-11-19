d6981852d4b0014f95f8070f33694070a496fe3c
showa_yojyo <yojyo@hotmail.com>
2019-11-19 17:04:29 +0900

    (wifiota) Mysterious regression

--- wifiota.py
+++ wifiota.py
@@ -1,44 +1,46 @@
 #!/usr/bin/env python
 
-"""Automate Free Wi-Fi confirmation
+"""Automate confirmation for Ota_City_Free_Wi-Fi_1
 """
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 
 FLETS_PORTAL_URL = 'http://www.e-flets.jp/'
 
-PAGE_1_CSS_SELECTOR = 'a[checkID="2300"] > h2 > img'
-PAGE_2_RADIO_ID = 'radio-use'
-PAGE_2_BUTTON_NAME = 'loginBtn'
+PAGE_1_CSS_SELECTOR = 'a[checkID="2300"]'
+PAGE_2_CSS_SELECTOR = 'a[checkID="2310"]'
+PAGE_3_RADIO_ID = 'radio-use'
+PAGE_3_BUTTON_NAME = 'loginBtn'
 
 def main():
     """Make the WebDriver connect to FLET'S Wi-Fi"""
 
     driver = webdriver.Edge()
+    driver.implicitly_wait(20)
     try:
         driver.get(FLETS_PORTAL_URL)
 
-        # Click the banner "Wi-Fi"
-        driver.find_element_by_css_selector(
-            PAGE_1_CSS_SELECTOR).click()
-
-        WebDriverWait(driver, 20).until(
-            expected_conditions.presence_of_element_located(
-            (By.NAME, PAGE_2_BUTTON_NAME)))
+        # Click "Wi-Fi" and then "Free Wi-Fi Internet"
+        for i in (PAGE_1_CSS_SELECTOR, PAGE_2_CSS_SELECTOR):
+            elem = driver.find_element_by_css_selector(i)
+            url = elem.get_attribute('href')
+            driver.get(url)
 
         # The checkbox "I agree with the terms of use"
-        agreed = driver.find_element_by_id(PAGE_2_RADIO_ID)
+        agreed = driver.find_element_by_id(PAGE_3_RADIO_ID)
         agreed.click()
         assert agreed.is_selected()
 
         # The button "To confirmation screen"
-        driver.find_element_by_name(PAGE_2_BUTTON_NAME).click()
+        driver.find_element_by_name(PAGE_3_BUTTON_NAME).click()
 
         wait = WebDriverWait(driver, 60)
         wait.until(expected_conditions.title_contains(
-            '大田区公式観光サイト'))
+            '大田区立図書館'))
+    except Exception:
+        breakpoint()
+        raise
     finally:
         driver.close()
 
