bc2b22768f71f52ad23f2d8afc288488c081cba9
showa_yojyo <yojyo@hotmail.com>
2019-12-07 11:12:25 +0900

    (mjnet) Properly parse command line

--- mjnet.py
+++ mjnet.py
@@ -4,6 +4,7 @@
 """
 
 import sys
+from argparse import ArgumentParser
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
@@ -11,22 +12,55 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 
+__version__ = '1.?'
+
 MJ_NET_URL = 'https://www.sega-mj.net/mjac_p'
 MJ_NET_URL_SIGN_IN = MJ_NET_URL + '/mjlogin/login.jsp'
 MJ_NET_URL_SIGN_OUT = MJ_NET_URL + '/FwdPage?page=logout'
 MJ_NET_URL_TOP_PAGE = MJ_NET_URL + '/FwdPage?page=top'
 MJ_NET_URL_PLAYER_DATA_PAGE = MJ_NET_URL + '/FwdPage?page=playdata&i=0'
 
-def main():
+def parse_args(args):
+    """Parse the command line parameters."
+
+    Returns:
+        An instance of argparse.ArgumentParser that stores the command line
+        parameters.
+    """
+
+    parser = ArgumentParser(
+        description='automation script for access to MJ.NET')
+    parser.add_argument('--version', action='version', version=__version__)
+
+    parser.add_argument(
+        'user_name',
+        help='account name for MJ.NET')
+    parser.add_argument(
+        'password',
+        help='passowrd for user_name')
+    parser.add_argument(
+        '-p', '--pause',
+        action='store_true',
+        default=False,
+        help='pause before closing browser')
+    parser.add_argument(
+        '--only-login',
+        action='store_true',
+        default=False,
+        help='only sign in MJ.NET')
+    return parser.parse_args(args or ['--help'])
+
+
+def run(args):
     """The function invoked when the program is executed"""
 
-    if len(sys.argv) < 3:
-        print("Usage: mjnet.py USER_NAME PASSWORD", file=sys.stderr)
-        sys.exit(2)
-
+    pause = args.pause
     driver = webdriver.Edge()
     driver.implicitly_wait(20)
-    sign_in(driver, sys.argv[1], sys.argv[2])
+    sign_in(driver, args.user_name, args.password)
+    if args.only_login:
+        return
+
     try:
         go_to_top(driver)
         go_to_play_data(driver)
@@ -34,6 +68,8 @@ def main():
         go_to_daily_record(driver)
         print('*'*80)
         print_current_contents(driver)
+        if pause:
+            input('Pause. Press any key to continue:')
         sign_out(driver)
     except Exception:
         breakpoint()
@@ -41,7 +77,7 @@ def main():
     finally:
         driver.close()
 
-def pause(func):
+def hook(func):
     """debugging"""
     def wrapper(driver):
         #print(func.__name__)
@@ -66,17 +102,17 @@ def sign_out(driver):
     """Sign out of MJ.NET"""
     driver.get(MJ_NET_URL_SIGN_OUT)
 
-@pause
+@hook
 def go_to_top(driver):
     """Move to the top page"""
     driver.get(MJ_NET_URL_TOP_PAGE)
 
-@pause
+@hook
 def go_to_play_data(driver):
     """Move to プレイデータ"""
     driver.get(MJ_NET_URL_PLAYER_DATA_PAGE)
 
-@pause
+@hook
 def go_to_tompu_pro(driver):
     """Move to プロ卓東風戦"""
 
@@ -90,7 +126,7 @@ def go_to_tompu_pro(driver):
     driver.get(links[1].get_attribute('href'))
 
 
-@pause
+@hook
 def go_to_daily_record(driver):
     """Move to デイリー戦績"""
 
@@ -103,6 +139,9 @@ def print_current_contents(driver):
     # trim the first lines and the last lines
     print('\n'.join(driver.find_element_by_css_selector(
         'div.common-wrap').text.splitlines()[3:-3]))
+
+def main(args=sys.argv[1:]):
+    sys.exit(run(parse_args(args)))
 
 if __name__ == "__main__":
     main()
