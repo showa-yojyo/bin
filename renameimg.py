6936e791ce196f04485d275bc25ea11cbbcf54c0
showa_yojyo <yojyo@hotmail.com>
2020-02-21 12:40:41 +0900

    (renameimg) Delete old files

--- renameimg.py
+++ renameimg.py
@@ -2,8 +2,8 @@
 
 import sys
 from hashlib import md5
-from os import rename
-from os.path import dirname, join, splitext
+from os import (remove, rename)
+from os.path import (dirname, join, splitext)
 
 def newname(data, oldname):
     h = md5()
@@ -12,19 +12,15 @@ def newname(data, oldname):
 
     oldbasename, extension = splitext(oldname)
     return join(dirname(oldname), newbasename + extension)
-    #return dir + '/' + basename + extension
-    #return oldname.replace(oldbasename, newbasename)
 
 def main(args=sys.argv[1:]):
-    #sys.exit(run(parse_args(args)))
-
-    for i in args:
-        with open(i, 'rb') as input:
+    for oldname in args:
+        with open(oldname, 'rb') as input:
             data = input.read()
-            name = newname(data, i)
-
-        rename(i, name)
-        #print(i, name)
+        try:
+            rename(oldname, newname(data, oldname))
+        except FileExistsError:
+            remove(oldname)
 
 if __name__ == '__main__':
     main()
