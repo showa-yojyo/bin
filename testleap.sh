8f2c5233559d2209dda1ec77b5550d970398ded0
showa_yojyo <yojyo@hotmail.com>
2020-06-03 18:30:52 +0900

    Add two very simple shell scripts

---
+++ testleap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Test if we're in a leap year (Bash Beginner's Guide)
+
+year=${1:=$(date +%Y)}
+
+if (( $year % 400 == 0 )) || ( (( $year % 4 == 0 )) && (( $year % 100 != 0)) ) ; then
+    echo "This is a leap year. Don't forget to charge the extra day!"
+else
+    echo "This is not a leap year."
+fi
+
+# 要点：
+# テスト式における (( arithmetic-expr ))
+# 演算子 &&, ||
