1292d7d75c47e60ea22e050778ad40bc14908689
showa_yojyo <yojyo@hotmail.com>
2021-03-24 12:27:51 +0900

    moeimg.py: run by cmdline.execute()

--- moeimg.py
+++ moeimg.py
@@ -5,16 +5,17 @@ No description.
 """
 
 import sys
-from scrapy import Request
+from scrapy import cmdline, Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+TARGET_DOMAIN = 'moeimg.net'
 
 class MoeImgNetCrawler(CrawlSpider):
     """No descrition"""
 
-    name = 'moeimg.net'
-    allowed_domains = ['moeimg.net']
+    name = TARGET_DOMAIN
+    allowed_domains = [TARGET_DOMAIN]
     custom_settings = {
         'DOWNLOAD_DELAY': 1,
     }
@@ -27,7 +28,7 @@ class MoeImgNetCrawler(CrawlSpider):
     def start_requests(self):
         """No descrition"""
 
-        url = getattr(self, 'tag', None)
+        url = self.tag
         yield Request(url, dont_filter=True)
 
     def parse_entry(self, response):
@@ -40,4 +41,7 @@ class MoeImgNetCrawler(CrawlSpider):
 
 
 if __name__ == '__main__':
-    print(f'Usage: scrapy runspider {sys.argv[0]} [options] -a tag=URL')
+    #cmdline.execute(f"scrapy runspider {sys.argv[0]} -a tag={sys.argv[1]} -O images.csv".split())
+    command_line = ["scrapy", "runspider"]
+    command_line.extend(sys.argv)
+    cmdline.execute(command_line)
