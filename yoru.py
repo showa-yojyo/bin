804094264696264ba30f3dd0cf3c5441ca4ce3d2
showa_yojyo <yojyo@hotmail.com>
2021-04-07 13:09:51 +0900

    yoru.py: replace yoru.sh

---
+++ yoru.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+No description.
+"""
+
+import sys
+from scrapy import cmdline, Request
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+TARGET_DOMAIN = 'eromanga-yoru.com'
+
+class EromangaYoruSpider(CrawlSpider):
+    """No descrition"""
+
+    name = TARGET_DOMAIN
+    allowed_domains = [TARGET_DOMAIN]
+    custom_settings = {
+        'DOWNLOAD_DELAY': 1,
+    }
+
+    rules = (
+        # a[@title="個別記事ページへ"]
+        Rule(LinkExtractor(restrict_xpaths='//header[@class="article-header"]/div/a'), callback='parse_article'),
+        # »
+        Rule(LinkExtractor(restrict_xpaths='(//a[@class="nextpostslink"])[1]')),
+    )
+
+    def start_requests(self):
+        """No descrition"""
+
+        url = self.tag
+        yield Request(url, dont_filter=True)
+
+    def parse_article(self, response):
+        """No descrition"""
+
+        post_url = response.url
+        images = response.xpath('//img[contains(@class, "size-full")]/@src').getall()
+        yield {
+            'article': post_url,
+            'images': images,}
+
+if __name__ == '__main__':
+    #cmdline.execute(f"scrapy runspider {sys.argv[0]} -a tag={sys.argv[1]} -O images.jl".split())
+    command_line = ["scrapy", "runspider"]
+    command_line.extend(sys.argv)
+    cmdline.execute(command_line)
