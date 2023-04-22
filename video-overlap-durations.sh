37da65cbe118a801c29a515b55eb4939991670a3
showa_yojyo <yojyo@hotmail.com>
2023-04-22 22:12:49 +0900

    video-overlap-durations.sh: add a new script (WIP)

---
+++ video-overlap-durations.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+declare -r ffprobe_options="
+    -hide_banner
+    -v error
+    -show_entries format=duration
+    -of default=noprint_wrappers=1:nokey=1
+    ";
+
+declare -a durations
+for i in *.mp4 ; do
+    durations+=($(ffprobe $ffprobe_options "$i"))
+done
+
+sum=$(echo ${durations[0]} - 1 | bc)
+echo $sum
+for ((i=1; i<${#durations[@]}; i++)); do
+    sum=$(echo $sum + ${durations[i]} - 1 | bc)
+    echo $sum
+done
+
+
