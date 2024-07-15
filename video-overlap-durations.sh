e0ee1b5b7413481e18e5f365ddab1e97a37be67e
showa_yojyo <2386769+showa-yojyo@users.noreply.github.com>
2024-07-15 13:24:49 +0900

    bash: use arrays for arguments

--- video-overlap-durations.sh
+++ video-overlap-durations.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-declare -r ffprobe_options="
+declare -ar ffprobe_options=(
     -hide_banner
     -v error
-    -show_entries format=duration
-    -of default=noprint_wrappers=1:nokey=1
-    ";
+    -show_entries "format=duration"
+    -of "default=noprint_wrappers=1:nokey=1")
 
 declare -a durations
 for i in *.mp4 ; do
-    durations+=($(ffprobe $ffprobe_options "$i"))
+    durations+=($(ffprobe "${ffprobe_options[@]}" "$i"))
 done
 
 sum=$(echo ${durations[0]} - 1 | bc)
@@ -18,5 +17,3 @@ for ((i=1; i<${#durations[@]}; i++)); do
     sum=$(echo $sum + ${durations[i]} - 1 | bc)
     echo $sum
 done
-
-
