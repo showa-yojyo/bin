6b7af5d0cd8647206184fa46e3172a2a80edf7f8
showa_yojyo <2386769+showa-yojyo@users.noreply.github.com>
2024-10-02 23:01:46 +0900

    mjnet_auto.py: type checking

--- mjnet_auto.py
+++ mjnet_auto.py
@@ -4,12 +4,17 @@
 Sign in MJ.NET and scrape today's result.
 """
 
+from collections.abc import MutableMapping
 from getpass import getpass
 import os.path
 import sys
-from scrapy import cmdline, Spider, FormRequest
-from scrapy.linkextractors import LinkExtractor
-from scrapy.shell import inspect_response
+from typing import Any, Iterator, Self
+
+from scrapy import cmdline, Request, Spider, FormRequest # type: ignore
+from scrapy.http import Response # type: ignore
+from scrapy.linkextractors import LinkExtractor # type: ignore
+from scrapy.selector import Selector # type: ignore
+from scrapy.shell import inspect_response # type: ignore
 import yaml
 
 DEFAULT_CREDENTIALS_PATH = (
@@ -40,7 +45,7 @@ class MjscoreSpider(Spider):
     allowed_domains = ['www.sega-mj.net']
     start_urls = [MJ_NET_URL_SIGN_IN]
 
-    def parse(self, response, **kwargs):
+    def parse(self: Self, response: Response, **kwargs) -> FormRequest:
         """Pass the login page of MJ.NET"""
 
         return FormRequest.from_response(
@@ -50,7 +55,7 @@ class MjscoreSpider(Spider):
                 'password': getattr(self, 'password')},
             callback=self._after_login)
 
-    def _after_login(self, response):
+    def _after_login(self: Self, response: Response) -> Iterator[Request]:
         """Nagivate to the top page"""
 
         self.logger.info('_after_login')
@@ -61,12 +66,12 @@ class MjscoreSpider(Spider):
         else:
             yield response.follow(MJ_NET_URL_SIGN_IN_DO, self._after_login)
 
-    def _top_page(self, response):
+    def _top_page(self: Self, response: Response) -> Iterator[Request]:
         """Navigate to the player data page"""
 
         yield response.follow(MJ_NET_URL_PLAYER_DATA_PAGE, self._play_data)
 
-    def _play_data(self, response):
+    def _play_data(self: Self, response: Response) -> Iterator[Request]:
         """Navigate to the page 一般卓東風戦 or プロ卓東風戦"""
 
         ext = LinkExtractor(restrict_text='東風戦')
@@ -83,18 +88,18 @@ class MjscoreSpider(Spider):
             link_index = 0
         yield response.follow(links[link_index].url, self._tompu_games)
 
-    def _tompu_games(self, response):
+    def _tompu_games(self: Self, response: Response) -> Iterator[Request]:
         """Navigate to the daily record page"""
 
         ext = LinkExtractor(restrict_text='デイリー戦績')
         links = ext.extract_links(response)
         yield response.follow(links[0].url, self.parse_daily_score)
 
-    def parse_daily_score(self, response):
+    def parse_daily_score(self: Self, response: Response) -> Iterator[Request]:
         """Scraping method"""
 
-        item = {}
-        selector = response.selector
+        item: MutableMapping[str, str] = {}
+        selector: Selector = response.selector
         parse_score(selector, item)
         parse_recent_level(selector, item)
         parse_history(selector, item)
@@ -109,7 +114,7 @@ class MjscoreSpider(Spider):
         else:
             self.logger.debug('倍満以上なし終了')
 
-    def parse_best_mahjong(self, response):
+    def parse_best_mahjong(self: Self, response: Response) -> None:
         """TODO: 跳満以上のアガリがある場合にはリンク先のスクリーンショットを保存する (very hard)
         """
         pass
@@ -120,7 +125,7 @@ class MjscoreSpider(Spider):
 
 # 【SCORE】
 # 合計SCORE:-197.0
-def parse_score(selector, item):
+def parse_score(selector: Selector, item: MutableMapping[str, str]) -> None:
     """Parse 【SCORE】"""
 
     value = selector.xpath('//font[text() = "【SCORE】"]/following::text()').get()
@@ -132,7 +137,7 @@ def parse_score(selector, item):
 
 # 【最終段位】
 # 四人打ち段位:魔神 幻球:7
-def parse_recent_level(selector, item):
+def parse_recent_level(selector: Selector, item: MutableMapping[str, str]) -> None:
     """Parse 【最終段位】"""
 
     value = selector.xpath('//font[text() = "【最終段位】"]/following::text()').get()
@@ -149,7 +154,7 @@ def parse_recent_level(selector, item):
 # 3rd|**---*--
 # 4th|---*--**
 # old         new
-def parse_history(selector, item):
+def parse_history(selector: Selector, item: MutableMapping[str, str]) -> None:
     """Parse 【m/dの最新8試合の履歴】"""
 
     title = selector.xpath('//font[contains(.,"履歴")]/text()').get().strip()
@@ -169,7 +174,7 @@ def parse_history(selector, item):
 # 平均順位:3.13
 #
 # プレイ局数:40局
-def parse_ranks(selector, item):
+def parse_ranks(selector: Selector, item: MutableMapping[str, str]) -> None:
     """Parse 【順位】"""
 
     values = selector.xpath('//font[contains(.,"順位")]/following::text()[position() < 8]').getall()
@@ -184,7 +189,7 @@ def parse_ranks(selector, item):
 # 平均アガリ翻:3.25翻
 # 平均アガリ巡目:12.75巡
 # 振込み率:20.00%(8/40)
-def parse_stats(selector, item):
+def parse_stats(selector: Selector, item: MutableMapping[str, str]) -> None:
     """Parse 【打ち筋】"""
 
     values = selector.xpath('//font[text() = "【打ち筋】"]/following::text()[position() < 5]').getall()
@@ -196,7 +201,7 @@ def parse_stats(selector, item):
 
 # 【3/9の最高役】--> //font[contains(.,"最高役")]/following-sibling::text()
 # 最高役のデータがありません。最高役は、跳満以上のアガリが対象となります。
-def parse_best(selector, item):
+def parse_best(selector: Selector, item: MutableMapping[str, str]) -> None:
     """Parse 【m/dの最高役】"""
 
     title = selector.xpath('//font[contains(.,"最高役")]/text()').get().strip()
@@ -210,7 +215,7 @@ def parse_best(selector, item):
     print(item['best'])
     print()
 
-def read_credentials():
+def read_credentials() -> Any:
     """Try to read the ID and password from
 
     #. `$XDG_CONFIG_HOME/mjnet_auto/mjnet_auto.yaml`,
@@ -230,7 +235,7 @@ def read_credentials():
         except OSError:
             pass
 
-def main():
+def main() -> None:
     """Receive account information and run spider"""
 
     game_mode = GAME_MODE_PROFESSIONAL
