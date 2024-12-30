db7b2481a54f3972adf99bdd3bc89f8784f1e27f
showa_yojyo <2386769+showa-yojyo@users.noreply.github.com>
2024-12-30 14:46:38 +0900

    2jipink.py: improve CLI powered by Click

--- 2jipink.py
+++ 2jipink.py
@@ -4,12 +4,20 @@
 No description.
 """
 
+from __future__ import annotations
+
 import sys
-from typing import Iterator, Mapping, Self
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Iterator, Mapping, Self
+
 from scrapy import cmdline, Request  # type: ignore
-from scarpy.http import Response  # type: ignore
+from scrapy.http import Response  # type: ignore
 from scrapy.linkextractors import LinkExtractor  # type: ignore
 from scrapy.spiders import CrawlSpider, Rule  # type: ignore
+
+import click
 
 TARGET_DOMAIN = "2ji.pink"
 
@@ -34,7 +42,7 @@ class Crawler(CrawlSpider):
     def start_requests(self: Self) -> Iterator[Request]:
         """No descrpition"""
 
-        url = self.tag
+        url = f"https://{TARGET_DOMAIN}/tag/{self.tag}"  # type: ignore[attr-defined]
         yield Request(url, dont_filter=True)
 
     def parse_entry(self: Self, response: Response) -> Iterator[Mapping]:
@@ -50,8 +58,44 @@ class Crawler(CrawlSpider):
             }
 
 
-if __name__ == "__main__":
-    # cmdline.execute(f"scrapy runspider {sys.argv[0]} -a tag={sys.argv[1]} -O images.csv".split())
-    command_line = ["scrapy", "runspider"]
-    command_line.extend(sys.argv)
+@click.command()
+@click.argument("tag", nargs=1, required=True)
+@click.option(
+    "-f",
+    "--format",
+    type=click.Choice(
+        ["json", "jsonline", "xml", "csv"],
+        case_sensitive=False,
+    ),
+    default="json",
+    help="scraped data format",
+)
+@click.option(
+    "--log/--nolog",
+    default=False,
+    help="enable logging",
+)
+@click.help_option(help="show this message and exit")
+def main(tag: str, format: str, log: bool) -> None:
+    """Crawl a website."""
+
+    command_line = [
+        "scrapy",
+        "runspider",
+        sys.argv[0],
+        # Pass `-a TAG`.
+        "-a",
+        f"tag={tag}",
+        # Pass `-o STDOUT:FORMAT`.
+        "-o",
+        f"-:{format}",
+    ]
+
+    if not log:
+        command_line.append("--nolog")
+
     cmdline.execute(command_line)
+
+
+if __name__ == "__main__":
+    main()
