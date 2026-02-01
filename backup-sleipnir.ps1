6d1fdd9293036ae9a77615862f98256c6c140bb3
showa_yojyo <2386769+showa-yojyo@users.noreply.github.com>
2026-02-01 12:13:37 +0900

    backup-{sleipnir,ssd}.ps1: new scripts not yet tested

---
+++ backup-sleipnir.ps1
@@ -0,0 +1,29 @@
+# A backup script for Sleipnir.
+# Usage: backup-sleipnir.ps1
+# Reference: <https://w.atwiki.jp/fenrirsleipnir/pages/207.html>
+
+# Note: To perform a real backup, comment out the DRY_RUN line.
+$DRY_RUN = "/L"
+
+$SLEIPNIR = "Fenrir Inc\Sleipnir5"
+$SRC_ROOT = "$env:APPDATA\${SLEIPNIR}%"
+$DEST_ROOT = "E:\backup\AppData\Roaming\${SLEIPNIR}"
+
+$ITEMS_TO_BACKUP = @(
+    "~temp\chrome\skins\default\widget\module\SearchEngine"
+    "setting\override"
+    "setting\modules\superdrag"
+    "setting\client\favorite3.json"
+    "setting\client\lastvisited.json"
+    "setting\client\recently-tab.json"
+    "setting\client\rebar.xml"
+    "setting\client\tab.json"
+    "setting\uri-action.json"
+    "setting\client\user.ini"
+    "setting\modules\ChromiumViewer\Default\Login Data"
+    "setting\modules\ChromiumViewer\Default"
+    )
+
+foreach ($i in $ITEMS_TO_BACKUP){
+    robocopy "${SRC_ROOT}\$i" "${DEST_ROOT}\$i" $DRY_RUN /MIR /E /ZB /COPYALL /DCOPY:t /XJ /R:0 /W:1
+}
---
+++ backup-ssd.ps1
@@ -0,0 +1,28 @@
+# A backup script of robocopy version.
+# Usage: backup-ssd.ps1
+
+# Note: To perform a real backup, manually set the $DRY_RUN variable to an empty
+# string.
+$DRY_RUN = "/L"
+
+# Back up %APPDATA% packages.
+
+$TARGETS = @(GIMP/3.0, inkscape, LibreOffice/4/user)
+$SRC_ROOT = $env:APPDATA
+$BACKUP_DEST_ROOT = "E:\backup"
+$DEST_ROOT = "${BACKUP_DEST_ROOT}\AppData\Roaming"
+
+foreach (i in $TARGETS){
+    robocopy "${SRC_ROOT}\$i" "${DEST_ROOT}\$i" $DRY_RUN /MIR /E /ZB /COPYALL /DCOPY:t /XJ /R:0 /W:1
+}
+
+# Back up %USERPROFILE% without folders.
+
+robocopy "$env:USERPROFILE" "${BACKUP_DEST_ROOT}" $DRY_RUN /LEV:1 /R:1 /W:1 /ZB /XF "NTUSER*" "*.lnk"
+
+# Back up %USERPROFILE% folders.
+
+$TARGETS = @(devel, Documents, Music, Pictures)
+foreach (i in $TARGETS){
+    robocopy "$env:USERPROFILE\$i" "${BACKUP_DEST_ROOT}\$i" $DRY_RUN /E /ZB /COPYALL /DCOPY:t /XJ /R:0 /W:1
+}
